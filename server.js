1a3610652bca1c21ae5a01c562b33794932e99da
Satheesh <mygithub2019@gmail.com>
2019-03-08 06:18:06 +0530

    buil for heroku

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
+app = express();
+
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname + '/dist/index.html')
+})
+
 app.listen(port);
+
+// var path = require('path');
+// var serveStatic = require('serve-static');
+
+
+
+
 console.log('server started '+ port);
